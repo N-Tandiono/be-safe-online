31d9de9936187a34cd1f9e4dd1508a282ffd4a9d
Nick <n.tandiono@yahoo.com>
2020-12-07 17:27:09 +1100

    Added info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="home-title">
+				<span>
+					<h1>Be Safe Online</h1>
+					<p>Learn about <b>staying safe online</b> in a <b>fun</b> and <b>engaging</b> way!</p>
+					<p>A <b>gamified solution</b> to teaching kids on the <b>dos and don'ts</b> of using the web</p>
+				</span>
+			</header>
+		</div>
+	);
 }
 
 export default App;
