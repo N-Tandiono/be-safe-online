a99974ac3a03149ff3d5ea19456f6f02edafb5e7
Nick <n.tandiono@yahoo.com>
2021-01-01 17:07:56 +1100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,107 +1,40 @@
 import './App.css';
 import Navigation from './components/navigation/navigation';
+import Home from './components/home/home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
 	return (
 		<>
-			<Navigation />
-			<div id="front-page" className="fill-screen-1">
-				<div className="home-container">
-					<header className="home-title">
-						<span>
-							<h1>Be Safe Online</h1>
-							<p>Learn about <b>staying safe online</b> in a <b>fun</b> and <b>engaging</b> way!</p>
-							<p>A <b>gamified solution</b> to teaching kids on the <b>dos and don'ts</b> of using the web</p>
-						</span>
-						<button id='explore'>Explore</button>
-						<a href="#purpose" className="scroll-down"></a>
-					</header>
-					<div id="purpose" onReset></div>
-				</div>
-			</div>
-			<div className="fill-screen-2">
-				<div className="home-container">
-					<header className="home-content">
-						<span>
-							<h1>Purpose</h1>
-							<p>There are constant threats on the internet. Sadly, Anti-virus and Firewalls do not always protect the user from these threats.</p>
-							<p>Stay Safe Online aims to educate users of these threats, reducing the risk exposure</p>
-						</span>
-					</header>
-				</div>
-			</div>
-			<div className="fill-screen-3">
-				<div className="home-container">
-					<header className="home-content">
-						<span>
-							<h1>Values</h1>
-							<div class='row'>
-								<div class='column'>
-									<div class='left-column'>
-										Effective Learning
-								</div>
-								</div>
-								<div class='column'>
-									<div class='right-column'>
-										Fun and Engaging
-								</div>
-								</div>
-							</div>
-							<div class='row'>
-								<div class='column'>
-									<div class='left-column'>
-										Gamified Solution
-								</div>
-								</div>
-								<div class='column'>
-									<div class='right-column'>
-										Life-Skills
-								</div>
-								</div>
-							</div>
-							<div class='row'>
-								<div class='column'>
-									<div class='left-column'>
-										For all Ages
-								</div>
-								</div>
-								<div class='column'>
-									<div class='right-column'>
-										Self-Paced
-								</div>
-								</div>
-							</div>
-							<div class='row'>
-								<div class='column'>
-									<div class='left-column'>
-										Real-life Experiences
-								</div>
-								</div>
-								<div class='column'>
-									<div class='right-column'>
-										Basics of Security
-								</div>
-								</div>
-							</div>
-						</span>
-					</header>
-				</div>
-			</div>
-			{/* <div className="fill-screen-4">
-				<div className="home-container">
-					<span>
-						<h1>Statistics</h1>
-						<p>Pie chart image</p>
-						<p>Talk about statistics</p>
-					</span>
-				</div>
-			</div> */}
-			<footer>
-				Okay this is the bottom, thanks :)
-				<br />
-				More for the Footer in the future
-			</footer>
+			<Router>
+				<Navigation />
+				<Switch>
+					<Route path="/about">
+
+					</Route>
+					<Route path="/contact">
+
+					</Route>
+					<Route path="/play">
+
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
 		</>
 	);
 }
+
+{/* <div className="fill-screen-4">
+	<div className="home-container">
+		<span>
+			<h1>Statistics</h1>
+			<p>Pie chart image</p>
+			<p>Talk about statistics</p>
+		</span>
+	</div>
+</div> */}
 
 export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,97 @@
+import './home.css';
+
+function Home() {
+	return (
+		<>
+			<div id="front-page" className="fill-screen-1">
+				<div className="home-container">
+					<header className="home-title">
+						<span>
+							<h1>Be Safe Online</h1>
+							<p>Learn about <b>staying safe online</b> in a <b>fun</b> and <b>engaging</b> way!</p>
+							<p>A <b>gamified solution</b> to teaching kids on the <b>dos and don'ts</b> of using the web</p>
+						</span>
+						<button id='explore'>Explore</button>
+						<a href="#purpose" className="scroll-down"></a>
+					</header>
+					<div id="purpose"></div>
+				</div>
+			</div>
+			<div className="fill-screen-2">
+				<div className="home-container">
+					<header className="home-content">
+						<span>
+							<h1>Purpose</h1>
+							<p>There are constant threats on the internet. Sadly, Anti-virus and Firewalls do not always protect the user from these threats.</p>
+							<p>Stay Safe Online aims to educate users of these threats, reducing the risk exposure</p>
+						</span>
+					</header>
+				</div>
+			</div>
+			<div className="fill-screen-3">
+				<div className="home-container">
+					<header className="home-content">
+						<span>
+							<h1>Values</h1>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										Effective Learning
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Fun and Engaging
+								</div>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										Gamified Solution
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Life-Skills
+								</div>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										For all Ages
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Self-Paced
+								</div>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										Real-life Experiences
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Basics of Security
+								</div>
+								</div>
+							</div>
+						</span>
+					</header>
+				</div>
+			</div>
+			<footer>
+				Okay this is the bottom, thanks :)
+				<br />
+				More for the Footer in the future
+			</footer>
+		</>
+	);
+}
+
+export default Home;
