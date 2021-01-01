f0df287322833eb83552597027025078e28629fc
Nick <n.tandiono@yahoo.com>
2021-01-01 16:48:21 +1100

    Front page changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
 import Navigation from './components/navigation/navigation';
-
 function App() {
 	return (
 		<>
 			<Navigation />
-			<div className="fill-screen-1">
-				<header className="home-title">
-					<span>
-						<h1>Be Safe Online</h1>
-						<p>Learn about <b>staying safe online</b> in a <b>fun</b> and <b>engaging</b> way!</p>
-						<p>A <b>gamified solution</b> to teaching kids on the <b>dos and don'ts</b> of using the web</p>
-					</span>
-					<button>Explore</button>
-					<a href="#about">
-						<div className="scroll-down"></div>
-					</a>
-				</header>
-			</div>
-			<section id='about'>
-				<div className="fill-screen-2">
+			<div id="front-page" className="fill-screen-1">
+				<div className="home-container">
 					<header className="home-title">
+						<span>
+							<h1>Be Safe Online</h1>
+							<p>Learn about <b>staying safe online</b> in a <b>fun</b> and <b>engaging</b> way!</p>
+							<p>A <b>gamified solution</b> to teaching kids on the <b>dos and don'ts</b> of using the web</p>
+						</span>
+						<button id='explore'>Explore</button>
+						<a href="#purpose" className="scroll-down"></a>
+					</header>
+					<div id="purpose" onReset></div>
+				</div>
+			</div>
+			<div className="fill-screen-2">
+				<div className="home-container">
+					<header className="home-content">
 						<span>
 							<h1>Purpose</h1>
 							<p>There are constant threats on the internet. Sadly, Anti-virus and Firewalls do not always protect the user from these threats.</p>
@@ -28,70 +28,76 @@ function App() {
 						</span>
 					</header>
 				</div>
-			</section>
+			</div>
 			<div className="fill-screen-3">
-				<span>
-					<h1>Values</h1>
-					<div class='row'>
-						<div class='column'>
-						<div class='left-column'>
-							Effective Learning
-						</div>
-						</div>
-						<div class='column'>
-						<div class='right-column'>
-							Fun and Engaging
-						</div>
-						</div>
-					</div>
-					<div class='row'>
-						<div class='column'>
-						<div class='left-column'>
-							Gamified Solution
-						</div>
-						</div>
-						<div class='column'>
-						<div class='right-column'>
-							Life-Skills
-						</div>
-						</div>
-					</div>
-					<div class='row'>
-						<div class='column'>
-						<div class='left-column'>
-							For all Ages
-						</div>
-						</div>
-						<div class='column'>
-						<div class='right-column'>
-							Self-Paced
-						</div>
-						</div>
-					</div>
-					<div class='row'>
-						<div class='column'>
-						<div class='left-column'>
-							Real-life Experiences
-						</div>
-						</div>
-						<div class='column'>
-						<div class='right-column'>
-							Basics of Security
-						</div>
-						</div>
-					</div>
-				</span>
+				<div className="home-container">
+					<header className="home-content">
+						<span>
+							<h1>Values</h1>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										Effective Learning
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Fun and Engaging
+								</div>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										Gamified Solution
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Life-Skills
+								</div>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										For all Ages
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Self-Paced
+								</div>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='column'>
+									<div class='left-column'>
+										Real-life Experiences
+								</div>
+								</div>
+								<div class='column'>
+									<div class='right-column'>
+										Basics of Security
+								</div>
+								</div>
+							</div>
+						</span>
+					</header>
+				</div>
 			</div>
-			<div className="fill-screen-4">
-				<span>
-					<h1>Statistics</h1>
-					<p>Pie chart image</p>
-					<p>Talk about statistics</p>
-				</span>
-			</div>
+			{/* <div className="fill-screen-4">
+				<div className="home-container">
+					<span>
+						<h1>Statistics</h1>
+						<p>Pie chart image</p>
+						<p>Talk about statistics</p>
+					</span>
+				</div>
+			</div> */}
 			<footer>
 				Okay this is the bottom, thanks :)
-				<br/>
+				<br />
 				More for the Footer in the future
 			</footer>
 		</>
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -3,9 +3,14 @@ import './navigation.css';
 function Navigation() {
 	return (
 		<div className="navigation">
-			<p>Logo, About Us, Contant Us, Play</p>
+			<ul>
+				<li><a href="#" id="logo">Logo</a></li>
+				<li><a href="#" id="about">About Us</a></li>
+				<li><a href="#" id="contact">Contant Us</a></li>
+				<li><a href="#" id="play">Play</a></li>
+			</ul>
 		</div>
-		
+
 	);
 }
 
